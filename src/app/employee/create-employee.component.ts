3c5bb53998a62b90b91a3998fcada75edd67ba7a
Jose <jose8055@gmail.com>
2019-10-05 14:50:41 -0400

    Subscribe ValueChanges

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,21 +8,25 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
 employeeForm: FormGroup;
-  constructor() { }
+fullNameLength = 0;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
 
-this.employeeForm = new FormGroup({
-fullName: new FormControl(),
-email: new FormControl(),
+    this.employeeForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      email: [''],
+      skills: this.fb.group({
+        skillName: [''],
+        experienceInYears: [''],
+        proficiency: ['beginner']
+      })
+    });
 
-skills: new FormGroup({
-  skillName: new FormControl(),
-  experienceInYears: new FormControl(),
-      proficiency: new FormControl()
-})
+    this.employeeForm.get('fullName').valueChanges.subscribe((value: string) => {
+      this.fullNameLength = value.length;
+    })
 
-});
   }
 
   onLoadDataClick(): void {
